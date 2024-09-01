ae31b068bacd88cce64471bf180386e704632d59
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-01 14:11:23 +0300

    adding Header

--- designit/src/App.js
+++ designit/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home/Home';
+import { Header } from './components/Header/Header';
 
 function App() {
+
   return (
     <div className="App">
+      <Header />
       <div className="main-content">
         <Routes>
           <Route path ="/" element={<Home />} />
