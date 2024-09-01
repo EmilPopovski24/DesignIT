b9847ef050f60ca503772623eb0849e210789172
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-01 14:03:15 +0300

    update index.js with BrowserRouter

--- designit/src/App.js
+++ designit/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <div className="main-content">
+        <Routes>
+          <Route path ="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ designit/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+
+
+export const Home = () => {
+    return(
+        <>
+        <p>Home page</p>
+        </>
+    )
+        
+    
+}
