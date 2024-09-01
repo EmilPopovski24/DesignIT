59f281307668dd716f6b6ff3b9d07f616c52826a
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-01 14:30:31 +0300

    Header update

--- designit/src/components/Header/Header.js
+++ designit/src/components/Header/Header.js
@@ -1,8 +1,19 @@
-
+import './Header.css';
 
 export const Header = () => {
 
     return(
-        <p>Header</p>
+        <>
+        <div className="header">
+            <ul className="header-ul">
+                <li className="header-li">Login</li>
+                <li className="header-li">Register</li>
+                <li className="header-li">Logout</li>
+                <li className="header-li">My Tasks</li>
+                <li className="header-li">Add New Task</li>
+                <li className="header-li">About</li>
+            </ul>
+        </div>
+        </>
     )
 }
