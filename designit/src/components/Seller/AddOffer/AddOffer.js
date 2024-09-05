d2489e731b2535d1e92c0bcb5b98a72b0c16f1ee
EmilPopovski24 <106330446+EmilPopovski24@users.noreply.github.com>
2024-09-05 13:18:15 +0300

    Update AddOffer.js

--- designit/src/components/Seller/AddOffer/AddOffer.js
+++ designit/src/components/Seller/AddOffer/AddOffer.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { useForm } from '../../hooks/useForm';
+import { useForm } from "../../../hooks/useForm";
 import { OfferContext } from "../../../contexts/OfferContext";
 import "./AddOffer.css";
 
@@ -19,29 +19,28 @@ export const AddOffer = () => {
         <>
         <h1 className='addOfferHeader'>Add Your Offer</h1>
             <section id ="create-page">
-                <form id="create" method="POST">
+                <form id="create" method="POST" onSubmit={onSubmit}>
                     <label htmlFor="type-title">Type</label>
-                    <input type="text" id="type" name="type"  placeholder="Type..." className='parameters' />
+                    <input type="text" id="type" name="type" values={values.type} onChange={changeHandler} placeholder="Type..." className='parameters' />
                     <hr />
                     <label htmlFor="task-title">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Offer name..." className='parameters'/>
+                    <input type="text" id="name" name="name" values={values.name} onChange={changeHandler} placeholder="Offer name..." className='parameters'/>
                     <hr />
                     <label htmlFor="image">Image URL</label> 
-                    <input type="imageUrl" alt="PetPicture" id="imageUrl" name="imageUrl"  placeholder="Image..." className='parameters' />
+                    <input type="imageUrl" alt="PetPicture" id="imageUrl" name="imageUrl"  values={values.imageUrl} onChange={changeHandler}placeholder="Image..." className='parameters' />
                     <hr />
                     <label htmlFor="description">Description</label>
-                    <textarea id="description" name="description"  placeholder="Description..." className='parameters' />
+                    <textarea id="description" name="description" values={values.description} onChange={changeHandler} placeholder="Description..." className='parameters' />
                     <hr />
                     <label htmlFor="colors">Colors</label>
-                    <input type="text" id="colors" name="colors"  placeholder="Colors..." className='parameters' />
+                    <input type="text" id="colors" name="colors" values={values.colors} onChange={changeHandler} placeholder="Colors..." className='parameters' />
                     <hr />
                     <label htmlFor="price">Price</label>
-                    <input type="text" id="price" name="price" placeholder="Price..." className='parameters' /> <p>BGN</p>
+                    <input type="text" id="price" name="price" values={values.price} onChange={changeHandler} placeholder="Price..." className='parameters' /> <p>BGN</p>
                     <hr />
                     <button type="submit" className="addPetBtn">Submit an Offer</button>
                 </form>
             </section>
         </>
-
     )
 }
